870e78d924da83f044bd8eca8c8e4ee7600b36f8
superskibidi <rookiecookierc@gmail.com>
2025-05-25 18:15:08 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,39 @@
-const CACHE_NAME = 'hda-breakfast';
+// service-worker.js
+const CACHE_NAME = 'hda-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/hero.mp4'
+  '/favicon.png',
+  '/touch-icon.png',
+  '/hero.mp4',
+  '/sizzle.mp3',
+  '/manifest.json',
 ];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
+    )
+  );
+});
